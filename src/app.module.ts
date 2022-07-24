04463642870e8ff4c5c4a7c1b0c69d2a29293348
JunKoding <wnsrb0407@daum.net>
2022-07-25 08:51:57 +0900

    feat: controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MoviesController } from './movies/movies.controller';
 
 
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [MoviesController],
   // controllers는 url을 가져오고 함수를 실행한다.
   providers: [],
 })
